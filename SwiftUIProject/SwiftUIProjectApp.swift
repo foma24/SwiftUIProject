5ae588496f2be6fd814f64910cb9ecc094da03eb
Denis Fomichev <fomichev.ds88@gmail.com>
2023-01-20 12:18:16 -0500

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Denis Fomichev on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
